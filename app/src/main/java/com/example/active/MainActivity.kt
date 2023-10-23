4e6ef197a16c71c94a6f12aba762ae50aa97171c
Lennox Nunu <lennoxnunu@gmail.com>
2023-10-23 13:37:19 +0200

    data binding with object starter

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.active
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.active.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val student = getStudent()
+        binding.nameText.text = student.name
+        binding.emailText.text = student.email
+    }
+
+    private fun getStudent(): Student {
+        return Student(1,"Alex","alex@gmail.com")
     }
 }
---
+++ app/src/main/java/com/example/active/Student.kt
@@ -0,0 +1,3 @@
+package com.example.active
+
+class Student(var id: Int, var name: String, var email: String)
