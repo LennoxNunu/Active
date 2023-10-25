fc17d071223cbaa1fdbc0531e222621f6e53babb
Lennox Nunu <lennoxnunu@gmail.com>
2023-10-25 13:54:37 +0200

    view model starter

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.active
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.active.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.countText.text = count.toString()
+        binding.button.setOnClickListener{
+            count++
+            binding.countText.text = count.toString()
+        }
     }
 }
