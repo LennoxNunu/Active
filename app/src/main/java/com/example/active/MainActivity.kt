bc6e988f740666a6ffa060423c70b3ce9485b954
Lennox Nunu <lennoxnunu@gmail.com>
2023-10-29 16:32:45 +0200

    two way data binding starter

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.active
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
+import com.example.active.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel:MainActivityViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
     }
 }
