7b5c1a24ba17c46f36d1b1c5dc056bb41107ba2e
LennoxNunu <lennoxnunu@gmail.com>
2023-11-06 21:29:45 +0200

    recycler view

---
+++ app/src/main/java/com/example/active/Fruit.kt
@@ -0,0 +1,3 @@
+package com.example.active
+
+data class Fruit(val name: String,val supplier: String)
--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.example.active
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import layout.MyRecyclerViewAdapter
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    val fruitsList = listOf<Fruit>(
+    Fruit("Apple","Tom"),
+    Fruit("Banana","Frank"),
+    Fruit("Mango","Joe"),
+    Fruit("Guava","Joe"),
+    Fruit("Lemon","Alex"),
+    Fruit("Orange","Alex"),
+    Fruit("Pear","Joe")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.myRecyclerView)
+        recyclerView.setBackgroundColor(Color.YELLOW)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyRecyclerViewAdapter(fruitsList) { selectedItem: Fruit ->
+            listItemClicked(selectedItem)
+        }
+    }
+
+    private fun listItemClicked(fruit:Fruit){
+        Toast.makeText(
+            this@MainActivity,
+            "Supplier is : ${fruit.supplier}",
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
