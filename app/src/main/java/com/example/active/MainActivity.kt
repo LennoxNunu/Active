151516f77c31271a7ee79d69b687a555c9294730
LennoxNunu <lennoxnunu@gmail.com>
2023-11-07 21:54:44 +0200

    Switch the thread of a coroutine

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -9,12 +9,14 @@ import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
     private lateinit var btnDownLoadUserData : Button
     private lateinit var btnCount : Button
     private lateinit var tvCount : TextView
+    private lateinit var tvUserMessage: TextView
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         btnDownLoadUserData = findViewById(R.id.btnDownloadUserData)
         btnCount = findViewById(R.id.btnCount)
         tvCount = findViewById(R.id.tvCount)
+        tvUserMessage = findViewById(R.id.tvUserMessage)
 
         btnCount.setOnClickListener {
             tvCount.text = count++.toString()
@@ -36,9 +39,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun downloadUserData() {
+    private suspend fun downloadUserData() {
         for(i in 1..200000){
-            Log.i("My Tag","Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main){
+                tvUserMessage.text = "Downloading user $i in ${Thread.currentThread().name}"
+            }
 
         }
     }
