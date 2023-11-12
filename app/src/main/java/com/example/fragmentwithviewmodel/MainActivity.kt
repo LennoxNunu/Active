dabe81f4aca9ba0b66d5d08ad011db1a2dda0673
LennoxNunu <lennoxnunu@gmail.com>
2023-11-12 19:56:50 +0200

    start point working

--- app/src/main/java/com/example/fragmentwithviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/fragmentwithviewmodel/MainActivity.kt
@@ -15,33 +15,4 @@ class MainActivity : AppCompatActivity() {
                 .commitNow()
         }
     }
-}package com.example.fragmentwithviewmodel
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.view.View
-import android.widget.ProgressBar
-import androidx.lifecycle.lifecycleScope
-import com.example.fragmentwithviewmodel.ui.main.MainFragment
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
-
-        lifecycleScope.launch {
-            var progressBar = findViewById<ProgressBar>(R.id.progressBar)
-            delay(5000)
-            progressBar.visibility = View.VISIBLE
-            delay(1000)
-            progressBar.visibility = View.GONE
-        }
-    }
 }
