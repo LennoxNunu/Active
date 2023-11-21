b53bd0345e3f794819301efd393c3cbe60e7dbb7
LennoxNunu <lennoxnunu@gmail.com>
2023-11-21 13:19:27 +0200

    set constraints

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.lifecycle.Observer
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 
@@ -20,8 +22,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOneTimeWorkRequest(){
         val textView = findViewById<TextView>(R.id.textView)
+
+        val constraints = Constraints.Builder()
+            .setRequiresCharging(true)
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
         val workManager:WorkManager = WorkManager.getInstance(applicationContext)
-        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java).build()
+
+        val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
+            .setConstraints(constraints).build()
+
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
