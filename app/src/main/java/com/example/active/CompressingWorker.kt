64242607ca081aa8b0ca12e72780d301e3be3577
LennoxNunu <lennoxnunu@gmail.com>
2023-11-21 17:20:28 +0200

    chaining

---
+++ app/src/main/java/com/example/active/CompressingWorker.kt
@@ -0,0 +1,28 @@
+package com.example.active
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Data
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
+
+class CompressingWorker(context: Context,params:WorkerParameters): Worker(context,params) {
+
+
+    override fun doWork(): Result {
+
+        try{
+
+            for (i: Int in 0..300){
+                Log.i("Mytag","Compressing $i")
+            }
+
+            return Result.success()
+        }catch (e:Exception){
+            return Result.failure()
+        }
+
+    }
+}
