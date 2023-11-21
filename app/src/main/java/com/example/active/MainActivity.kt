3b161fbd81bd37d716afd11a70eba17b27154fdc
LennoxNunu <lennoxnunu@gmail.com>
2023-11-21 16:20:53 +0200

    set input output data of a worker

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.lifecycle.Observer
-import androidx.work.Constraints
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
+import androidx.work.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        const val KEY_COUNT_VALUE = "key_count"
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,20 +27,31 @@ class MainActivity : AppCompatActivity() {
     private fun setOneTimeWorkRequest(){
         val textView = findViewById<TextView>(R.id.textView)
 
+        val data: Data = Data.Builder()
+            .putInt(KEY_COUNT_VALUE,125)
+            .build()
+
         val constraints = Constraints.Builder()
-            .setRequiresCharging(true)
+            .setRequiresCharging(false)
             .setRequiredNetworkType(NetworkType.CONNECTED)
             .build()
 
         val workManager:WorkManager = WorkManager.getInstance(applicationContext)
 
         val uploadRequest = OneTimeWorkRequest.Builder(UploadWorker::class.java)
-            .setConstraints(constraints).build()
+            .setConstraints(constraints)
+            .setInputData(data)
+            .build()
 
         workManager.enqueue(uploadRequest)
         workManager.getWorkInfoByIdLiveData(uploadRequest.id)
             .observe(this, Observer {
                 textView.text = it.state.name
+                if(it.state.isFinished){
+                    val data = it.outputData
+                    val message = data.getString(UploadWorker.KEY_WORKER)
+                    Toast.makeText(applicationContext,message,Toast.LENGTH_LONG).show()
+                }
             })
     }
 }
