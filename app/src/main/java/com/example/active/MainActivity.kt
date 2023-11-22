29e3ba1d1431876fba33c1c015a526798f73f1a0
LennoxNunu <lennoxnunu@gmail.com>
2023-11-22 21:13:32 +0200

    fieldInjectionWithDagger2

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -2,17 +2,19 @@ package com.example.active
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DaggerSmartPhoneComponent.create()
-            .getSmartPhone()
-            .makeACallWithRecording()
+        DaggerSmartPhoneComponent.create().inject(this)
+
+        smartPhone.makeACallWithRecording()
 
     }
 }
