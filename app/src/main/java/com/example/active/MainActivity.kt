969cbf8c2045f210fbf9a919bd10a840f844cf8c
LennoxNunu <lennoxnunu@gmail.com>
2023-11-22 22:22:26 +0200

    application class

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -12,10 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-      DaggerSmartPhoneComponent.builder()
-          .memoryCardModule(MemoryCardModule(1000))
-          .build()
-          .inject(this)
+        (application as SmartPhoneApplication).smartPhoneComponent
+            .inject(this)
 
     }
 }
