571374acab6660dcd7a8b9eb0e024ea8b0446c24
LennoxNunu <lennoxnunu@gmail.com>
2023-11-22 21:47:39 +0200

    state of a module

--- app/src/main/java/com/example/active/MainActivity.kt
+++ app/src/main/java/com/example/active/MainActivity.kt
@@ -12,9 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        DaggerSmartPhoneComponent.create().inject(this)
-
-        smartPhone.makeACallWithRecording()
+      DaggerSmartPhoneComponent.builder()
+          .memoryCardModule(MemoryCardModule(1000))
+          .build()
+          .inject(this)
 
     }
 }
